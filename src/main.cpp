0e6d96f3de1bb83b2d5174ef4d8179c370a2666a
TioT2 <piopi4112@gmail.com>
2024-08-21 22:42:00 +0300

    Turn to client-server (daemon-client) architecture.

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,58 @@
 #include <string.h>
 #include <assert.h>
 #include <math.h>
+#include <stdint.h>
 
-// #include <signal.h>
+#include <windows.h>
+#include <TlHelp32.h>
 
-#include "cli/cli.h"
-#include "sqs/test/sqs_test.h"
+typedef enum __CliExecutionMode {
+  CLI_EXECUTION_MODE_DAEMON,
+  CLI_EXECUTION_MODE_CLIENT,
+} CliExecutionMode;
 
-// void signalHandler( int signal ) {
-//   static const struct {
-//     const char *name;
-//     int signal;
-//   } signalNameTable[] = {
-//     {"SIGINT"  , SIGINT  },
-//     {"SIGILL"  , SIGILL  },
-//     {"SIGFPE"  , SIGFPE  },
-//     {"SIGSEGV" , SIGSEGV },
-//     {"SIGTERM" , SIGTERM },
-//     {"SIGBREAK", SIGBREAK},
-//     {"SIGABRT" , SIGABRT },
-//   };
-// 
-//   const char *signalName = "UNKNOWN";
-// 
-//   for (uint32_t i = 0, n = sizeof(signalNameTable) / sizeof(signalNameTable[0]); i < n; i++) {
-//     if (signalNameTable[i].signal == signal) {
-//       signalName = signalNameTable[i].name;
-//       break;
-//     }
-//   }
-// 
-//   printf(
-//     CLI_SET_FOREGROUND_COLOR(255, 0, 0)
-//     "!!SIGNAL: %s\n"
-//     CLI_RESET_COLOR(),
-//     signalName
-//   );
-//   exit(0);
-// }
+#define INVALID_PROCESS_ID (~0U)
+
+uint32_t findDaemonProcessId( const wchar_t *name ) {
+  HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  uint32_t pid = INVALID_PROCESS_ID;
+
+  PROCESSENTRY32 entry;
+  entry.dwSize = sizeof(entry);
+
+  do {
+    if (lstrcmpW(entry.szExeFile, name) == 0) {
+      pid = entry.th32ProcessID;
+      break;
+    }
+  } while (Process32Next(handle, &entry));
+
+  CloseHandle(handle);
+  return pid;
+}
+
+int
+cliRunClient( void ) {
+  uint32_t daemonPid = findDaemonProcessId(L"ss_sqs.exe");
+  if (daemonPid == INVALID_PROCESS_ID) {
+    printf("Can't find daemon PID\n");
+  } else {
+    printf("Daemon PID: %d\n", daemonPid);
+  }
+
+  return 0;
+}
+
+int
+cliRunDaemon( void ) {
+  printf("PID: %d\n", GetCurrentProcessId());
+
+  while (TRUE) {
+    __noop;
+  }
+
+  return 0;
+}
 
 //----------------------------------------------------------------
 //! @brief project entry point
@@ -54,115 +69,25 @@ main(
   int argc,
   const char **argv
 ) {
+  CliExecutionMode executionMode = CLI_EXECUTION_MODE_CLIENT;
 
-
-  struct {
-    SqsBool enableMenu;
-    SqsBool enableHelp;
-  } config = {
-    .enableMenu  = SQS_TRUE,
-    .enableHelp  = SQS_FALSE,
-  };
-
-  CliParameterIterator parameterContext;
-
-  // Initialize parameter 'iterator'
-  cliInitParameterIterator(argc, argv, &parameterContext);
-
-  // Skip file path
-  cliParameterIteratorNext(&parameterContext);
-
-  const char *arg = NULL;
-
-  while ((arg = cliParameterIteratorNext(&parameterContext)) != NULL) {
-
-    if (strcmp(arg, "--nomenu") == 0) {
-      config.enableMenu = SQS_FALSE;
+  for (uint32_t argI = 1; argI < (uint32_t)argc; argI++) {
+    if (strcmp(argv[argI], "-d") == 0) {
+      executionMode = CLI_EXECUTION_MODE_DAEMON;
       continue;
     }
 
-    if (strcmp(arg, "--help") == 0) {
-      config.enableHelp = SQS_TRUE;
+    if (strcmp(argv[argI], "-c") == 0) {
+      executionMode = CLI_EXECUTION_MODE_CLIENT;
       continue;
     }
-
-    if (strcmp(arg, "--custom") == 0) {
-      SqsQuadraticEquationCoefficents coefs = { NAN, NAN, NAN };
-
-      struct {
-        const char *pStr;
-        float *p;
-      } cpc[] = {
-        {cliParameterIteratorNext(&parameterContext), &coefs.a},
-        {cliParameterIteratorNext(&parameterContext), &coefs.b},
-        {cliParameterIteratorNext(&parameterContext), &coefs.c},
-      };
-
-      // parse parameters
-      for (int i = 0; i < 3; i++) {
-        if (cpc[i].pStr == NULL) {
-          printf(
-            CLI_SET_FOREGROUND_COLOR(255, 0, 0)
-            "NO ENOUGH PARAMETERS FOR CLI QUADRATIC EQUATION\n"
-            CLI_RESET_COLOR()
-          );
-          return 0;
-        }
-
-        if (sscanf_s(cpc[i].pStr, "%f", cpc[i].p) != 1) {
-          printf(
-            CLI_SET_FOREGROUND_COLOR(255, 0, 0)
-            "INVALID FLOATING POINT PARAMETER: %s\n"
-            CLI_RESET_COLOR(),
-            cpc[i].pStr
-          );
-        }
-      }
-
-      if (!sqsValidateQuadraticEquationCoefficents(&coefs)) {
-        printf(
-          CLI_SET_FOREGROUND_COLOR(255, 0, 0)
-          "INVALID COEFFICENTS\n"
-          CLI_RESET_COLOR()
-        );
-        return 0;
-      }
-
-      SqsQuadraticSolution solution = { SQS_QUADRATIC_SOLVE_STATUS_NO_ROOTS };
-
-      sqsSolveQuadratic(&coefs, &solution);
-
-      printf("Solution: ");
-      sqsPrintQuadraticSolution(stdout, &solution);
-      putchar('\n');
-
-      continue;
-    }
-
-    printf(
-      CLI_SET_FOREGROUND_COLOR(255, 0, 0)
-      "UNKNOWN KEY \"%s\"\n"
-      CLI_RESET_COLOR(),
-      arg
-    );
-    return 0;
   }
 
-  // signal(SIGINT  , signalHandler);
-  // signal(SIGILL  , signalHandler);
-  // signal(SIGFPE  , signalHandler);
-  // signal(SIGSEGV , signalHandler);
-  // signal(SIGTERM , signalHandler);
-  // signal(SIGBREAK, signalHandler);
-  // signal(SIGABRT , signalHandler);
-
-  if (config.enableHelp)
-    cliRunHelp();
-
-  if (config.enableMenu)
-    cliRunMenu();
-
-  return 0;
+  switch (executionMode) {
+  case CLI_EXECUTION_MODE_DAEMON : return cliRunDaemon();
+  case CLI_EXECUTION_MODE_CLIENT : return cliRunClient();
+  default                        : return 1;
+  }
 } // main function end
 
 // sqs_main.c file end
